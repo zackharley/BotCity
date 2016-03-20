ecd66609b51b24bf9f6421b1921b0412f5183975
zharley18 <zackharley@gmail.com>
2016-03-20 12:41:03 -0400

    Add MAC address for bluetooth controller

--- Lab 4/Module_9/Module_9/Module_9.ino
+++ Lab 4/Module_9/Module_9/Module_9.ino
@@ -1,3 +1,5 @@
+// MAC Address: 201505057960
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -5,9 +7,11 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  while(!Serial.available());
+  if(Serial.available()==1)
+  {
   char c = Serial.read();
+  Serial.println(c);
   if(c=='Y')
     Serial.println("Hello World!");
-    
+  } 
 }
